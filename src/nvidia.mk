d4e4ae86b5cff7d1124ea0f46210ac14e2332951
Maykon Marcos Junior <152006628+maykonmjunior@users.noreply.github.com>
2025-03-31 22:32:21 -0300

    Create nvidia.mk

---
+++ src/nvidia.mk
@@ -0,0 +1,9 @@
+# https://wiki.archlinux.org/title/NVIDIA
+all:
+  echo "not working yet"
+  #git clone https://github.com/NVIDIA/open-gpu-kernel-modules
+  #cd open-gpu-kernel-modules/
+  #make modules -j$(nproc)
+  #make modules_install -j$(nproc)
+
+clean:
