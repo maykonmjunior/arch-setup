bca5868609e6a05a57abd51f2f1928b957c1b650
Maykon Marcos Junior <152006628+maykonmjunior@users.noreply.github.com>
2025-04-01 21:50:26 -0300

    Update main.mk

--- main.mk
+++ main.mk
@@ -1,24 +1,26 @@
 apps=(
-  "appimage_launcher"
-  "audacious"
-  "brave"
-  "discord"
-  "firefox"
+  "yay"
   "gmail"
   "nvidia"
   "orange"
   "rust"
-  "vscode"
   "whatsapp"
-  "yt-dlp"
 )
 
 all:
+  mkdir ~/Downloads
+  mkdir ~/Downloads/Wine
+  mkdir ~/Downloads/AppImage
+  mkdir ~/Github
+  mkdir ~/Music
+  mkdir ~/Pictures
+  yay
+  yay -S --needed - < apps.list
+
+yay:
+  sudo pacman -S --needed git base-devel
+  cd /opt
+  sudo git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin
+  makepkg -sirc
   cd ~
-  mkdir .reps
-  mkdir .AppImage
-  mkdir Github
-  mkdir Music
-  mkdir Pictures
-  cd .reps/
-  
