0031c3a3d3a859acae32e30d71a84e5e73923ffe
Maykon Marcos Junior <maykon.marcos.junior@grad.ufsc.br>
2025-04-06 21:26:41 -0300

    adding theme configurations

--- src/hyprland.mk
+++ src/hyprland.mk
@@ -1,7 +1,8 @@
 all:
 	mkdir -p ~/.config/waybar
 	mkdir -p /etc/xdg/waybar
-	sudo ln -sf "$(pwd)"/config/waybar/* /etc/xdg/waybar/*
+	sudo ln -sf "$(pwd)"/config/waybar/config.jsonc /etc/xdg/waybar/config.jsonc
+	sudo ln -sf "$(pwd)"/config/waybar/style.css /etc/xdg/waybar/style.css
 	sudo ln -sf "$(pwd)"/config/hypr/hyprlux.toml /etc/hyprlux/config.toml
 	sudo ln -sf "$(pwd)"/config/hypr/hyprland.conf /etc/hypr/hyprland.conf
 	sudo ln -sf "$(pwd)"/config/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
