2650bb45c5bc769579ec0b0b51805cee6db26a31
Maykon Marcos Junior <maykon.marcos.junior@grad.ufsc.br>
2025-04-09 14:18:26 -0300

    firefox shortcuts (attempt)

--- src/firefox.mk
+++ src/firefox.mk
@@ -3,4 +3,6 @@
 # https://superuser.com/questions/1271147/change-key-bindings-keyboard-shortcuts-in-firefox-57
 #TODO: setting of config files
 all:
-	@echo "not implemented yet"
+	mkdir -p ~/.mozilla/firefox/abc123.default-release/chrome
+	ln -sf ~/arch-setup/config/firefox/user.js ~/.mozilla/firefox/*.default/user.js
+	ln -sf ~/arch-setup/config/firefox/userChrome.css ~/.mozilla/firefox/*.default/chrome/userChrome.css
