27f0adfa7e44aa15d4a316aeb903981d0114772d
Maykon Marcos Junior <maykon.marcos.junior@grad.ufsc.br>
2025-04-09 14:19:40 -0300

    giving up firefox

--- src/firefox.mk
+++
@@ -1,8 +0,0 @@
-# https://support.mozilla.org/en-US/kb/customizing-firefox-using-autoconfig
-# https://support.mozilla.org/en-US/kb/keyboard-shortcuts-perform-firefox-tasks-quickly?redirectslug=Keyboard+shortcuts&redirectlocale=en-US
-# https://superuser.com/questions/1271147/change-key-bindings-keyboard-shortcuts-in-firefox-57
-#TODO: setting of config files
-all:
-	mkdir -p ~/.mozilla/firefox/abc123.default-release/chrome
-	ln -sf ~/arch-setup/config/firefox/user.js ~/.mozilla/firefox/*.default/user.js
-	ln -sf ~/arch-setup/config/firefox/userChrome.css ~/.mozilla/firefox/*.default/chrome/userChrome.css
