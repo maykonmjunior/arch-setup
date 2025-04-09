2af920223ceb7db119ebd2102ea99fa63a9fd3a8
Maykon Marcos Junior <maykon.marcos.junior@grad.ufsc.br>
2025-04-09 08:47:40 -0300

    adding keyboard config

--- src/gnome.mk
+++ src/gnome.mk
@@ -1,5 +1,11 @@
 all:
 	dconf load / < ~/arch-setup/config/dconf/user.txt
+	mkdir -p ~/.config/environment.d/
+	cp ~/arch-setup/config/environment.d/ibus.conf ~/.config/environment.d/ibus.conf
+	export GTK_IM_MODULE=ibus
+	export QT_IM_MODULE=ibus
+	export XMODIFIERS=@im=ibus
+	export INPUT_METHOD=ibus
 
 dump:
 	dconf dump / > ~/arch-setup/config/dconf/user.txt
