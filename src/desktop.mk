645fd5d79554388e0227e4bce45c9630dc8fbc9d
maykonmjunior <maykonmjunior@gmail.com>
2025-06-08 00:03:17 -0300

    Adding personal fonts

--- src/desktop.mk
+++ src/desktop.mk
@@ -2,3 +2,6 @@ all:
 	chmod +x config/notion/patch-notion-enhanced.linux.sh
 	sudo ~/arch-setup/config/notion/patch-notion-enhanced.linux.sh
 	ln -sf ~/arch-setup/config/notion/.notion-enhancer ~/.notion-enhancer
+	sudo mkdir -p /usr/local/share/fonts
+	sudo cp ~/arch-setup/fonts/* /usr/local/share/fonts
+	sudo fc-cache -vf
