0bdf6af41f70420501349c0aa08eb057b55763ac
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2025-06-09 21:55:34 -0300

    adding args and simbolic links

--- src/desktop.mk
+++ src/desktop.mk
@@ -1,11 +1,10 @@
 all:
 	systemctl --user enable --now gmail-tray.service
-	chmod +x config/notion/patch-notion-enhanced.linux.sh
-	sudo ~/arch-setup/config/notion/patch-notion-enhanced.linux.sh
-	ln -sf ~/arch-setup/config/notion/.notion-enhancer ~/.notion-enhancer
 	sudo mkdir -p /usr/local/share/fonts
 	sudo cp ~/arch-setup/fonts/* /usr/local/share/fonts
 	sudo fc-cache -vf
 	ln -sf ~/arch-setup/icons ~/.local/share/
-	cp ~/arch-setup/config/dconf/*.desktop ~/.local/share/applications/
+	ln -sf ~/arch-setup/config/dconf/*.desktop ~/.local/share/applications/
+	ln -sf ~/arch-setup/config/dconf/discord.desktop ~/.config/autostart/
+	ln -sf ~/arch-setup/config/dconf/notion.desktop ~/.config/autostart/
 
