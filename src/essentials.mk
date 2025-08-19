0093267eb7598caf6e27feeb7c79de1a9684c0cb
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2025-08-19 16:34:17 -0300

    always create a new ssh

--- src/essentials.mk
+++ src/essentials.mk
@@ -10,6 +10,11 @@ all:
 	export INPUT_METHOD=ibus
 	systemctl enable gdm.service
 	systemctl --user enable --now pipewire pipewire-pulse wireplumber
+	systemctl enable bluetooth.service
+	sudo usermod -aG video $USER
+	sudo rmmod uvcvideo
+	sudo modprobe -r uvcvideo
+	sudo modprobe uvcvideo
 	mkdir -p ~/.config/systemd/user
 	ln -sf ~/arch-setup/config/terminal/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
 	ln -sf ~/arch-setup/config/terminal/ssh-add.service ~/.config/systemd/user/ssh-add.service
@@ -19,15 +24,11 @@ all:
 	systemctl --user enable --now ssh-agent.service
 	systemctl --user enable --now ssh-add.service
 	git remote set-url origin git@github.com:maykonmjunior/arch-setup.git
+	read -p "insira o email para a ssh" email
+	ssh-keygen -t ed25519 -C "$email"
+	cat ~/.ssh/id_ed25519.pub
+	echo "Copie o resultado anterior e cole em: https://github.com/settings/ssh/new"
 	eval "$(ssh-agent -s)"
-	read -p "Baixou as chaves ssh do bkp no drive (y/n) " confirm
-	if [[ $confirm == "y" || $confirm == "Y" ]]; then
-		chmod 600 ~/.ssh/*/id_ed25519
-		ssh-add ~/.ssh/*/id_ed25519
-	fi
-	systemctl enable bluetooth.service
-	sudo usermod -aG video $USER
-	sudo rmmod uvcvideo
-	sudo modprobe -r uvcvideo
-	sudo modprobe uvcvideo
+	chmod 600 ~/.ssh/id_ed25519
+	ssh-add ~/.ssh/id_ed25519
 	
