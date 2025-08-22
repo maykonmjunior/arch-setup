4cc26e6215e4dd2ea214915d051a58b62bd0b4b7
maykonmjunior <maykonmjunior@gmail.com>
2025-08-22 17:17:39 -0300

    adding a config backup

--- src/desktop.mk
+++ src/desktop.mk
@@ -7,4 +7,16 @@ all:
 	ln -sf ~/arch-setup/config/dconf/*.desktop ~/.local/share/applications/
 	ln -sf ~/arch-setup/config/dconf/discord.desktop ~/.config/autostart/
 	ln -sf ~/arch-setup/config/dconf/notion.desktop ~/.config/autostart/
+	mkdir -p ~/MEGA/Main
+	mkdir -p ~/MEGA/Hobby
+	mkdir -p ~/MEGA/UFSC
+	mkdir -p ~/.mozilla/firefox/Main/
+	mkdir -p ~/.mozilla/firefox/Hobby/
+	mkdir -p ~/.mozilla/firefox/UFSC/
+# 	ln -sf ~/.mozilla/firefox/Main/prefs.js  ~/MEGA/Main/
+# 	ln -sf ~/.mozilla/firefox/Hobby/prefs.js ~/MEGA/Hobby/
+# 	ln -sf ~/.mozilla/firefox/UFSC/prefs.js  ~/MEGA/UFSC/
+	cp  ~/MEGA/Main/ ~/.mozilla/firefox/Main/prefs.js
+	cp ~/MEGA/Hobby/ ~/.mozilla/firefox/Hobby/prefs.js
+	cp  ~/MEGA/UFSC/ ~/.mozilla/firefox/UFSC/prefs.js
 
