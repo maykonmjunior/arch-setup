6e6795aebaae0ed41c4f3e6876cc85589ff8dca3
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2025-08-22 21:25:21 -0300

    adding fetchmail setup

--- src/desktop.mk
+++ src/desktop.mk
@@ -2,6 +2,7 @@ all:
 	systemctl --user enable --now gmail-tray.service
 	sudo mkdir -p /usr/local/share/fonts
 	sudo cp ~/arch-setup/fonts/* /usr/local/share/fonts
+	cp ~/arch-setup/config/tray/.fetchmailrc ~/.fetchmailrc
 	sudo fc-cache -vf
 	ln -sf ~/arch-setup/icons ~/.local/share/
 	ln -sf ~/arch-setup/config/dconf/*.desktop ~/.local/share/applications/
