289dfe06019740af5ce7890d64ac5c51fa54a5ec
maykonmjunior <maykonmjunior@gmail.com>
2025-09-19 10:34:02 -0300

    adding the backup to github

--- config/terminal/.bash_aliases
+++ config/terminal/.bash_aliases
@@ -1,4 +1,5 @@
 alias list='cat ~/.bash_aliases'
 alias upd='sudo pacman -Suy --noconfirm && yay -Suy --noconfirm && sudo pacman -Qdtq | sudo pacman -Qdtq | sudo xargs -r pacman -Rns --noconfirm'
 alias get_music='yt-dlp --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --output "%(title)s.%(ext)s"'
-alias bkp="make -f arch-setup/src/desktop.mk backup"
+alias bkp="make -f arch-setup/src/desktop.mk backup
+make -f arch-setup/src/gnome.mk dump"
--- src/desktop.mk
+++ src/desktop.mk
@@ -9,44 +9,44 @@ all:
 	ln -sf ~/arch-setup/config/dconf/discord.desktop ~/.config/autostart/
 	ln -sf ~/arch-setup/config/dconf/notion.desktop ~/.config/autostart/
 	ln -sf ~/arch-setup/config/dconf/wasistlos.conf ~/.config/wasistlos/settings.conf
-	mkdir -p ~/MEGA/Main
-	mkdir -p ~/MEGA/Hobby
-	mkdir -p ~/MEGA/UFSC
+# 	mkdir -p ~/arch-setup/config/firefox/Profiles/Main
+# 	mkdir -p ~/arch-setup/config/firefox/Profiles/Hobby
+# 	mkdir -p ~/arch-setup/config/firefox/Profiles/UFSC
 	mkdir -p ~/.mozilla/firefox/Main/
 	mkdir -p ~/.mozilla/firefox/Hobby/
 	mkdir -p ~/.mozilla/firefox/UFSC/
 
-	cp  ~/MEGA/Main/prefs.js  ~/.mozilla/firefox/Main/prefs.js
-	cp ~/MEGA/Hobby/prefs.js ~/.mozilla/firefox/Hobby/prefs.js
-	cp  ~/MEGA/UFSC/prefs.js  ~/.mozilla/firefox/UFSC/prefs.js
-	cp  ~/MEGA/Main/addons.json  ~/.mozilla/firefox/Main/addons.json
-	cp ~/MEGA/Hobby/addons.json ~/.mozilla/firefox/Hobby/addons.json
-	cp  ~/MEGA/UFSC/addons.json  ~/.mozilla/firefox/UFSC/addons.json
-	cp  ~/MEGA/Main/extensions.json  ~/.mozilla/firefox/Main/extensions.json
-	cp ~/MEGA/Hobby/extensions.json ~/.mozilla/firefox/Hobby/extensions.json
-	cp  ~/MEGA/UFSC/extensions.json  ~/.mozilla/firefox/UFSC/extensions.json
-	cp  ~/MEGA/Main/extension-settings.json ~/.mozilla/firefox/Main/extension-settings.json 
-	cp ~/MEGA/Hobby/extension-settings.json ~/.mozilla/firefox/Hobby/extension-settings.json
-	cp  ~/MEGA/UFSC/extension-settings.json ~/.mozilla/firefox/UFSC/extension-settings.json 
-	cp  ~/MEGA/Main/extension-preferences.json ~/.mozilla/firefox/Main/extension-preferences.json 
-	cp ~/MEGA/Hobby/extension-preferences.json ~/.mozilla/firefox/Hobby/extension-preferences.json
-	cp  ~/MEGA/UFSC/extension-preferences.json ~/.mozilla/firefox/UFSC/extension-preferences.json 
+	cp  ~/arch-setup/config/firefox/Profiles/Main/prefs.js  ~/.mozilla/firefox/Main/prefs.js
+	cp ~/arch-setup/config/firefox/Profiles/Hobby/prefs.js ~/.mozilla/firefox/Hobby/prefs.js
+	cp  ~/arch-setup/config/firefox/Profiles/UFSC/prefs.js  ~/.mozilla/firefox/UFSC/prefs.js
+	cp  ~/arch-setup/config/firefox/Profiles/Main/addons.json  ~/.mozilla/firefox/Main/addons.json
+	cp ~/arch-setup/config/firefox/Profiles/Hobby/addons.json ~/.mozilla/firefox/Hobby/addons.json
+	cp  ~/arch-setup/config/firefox/Profiles/UFSC/addons.json  ~/.mozilla/firefox/UFSC/addons.json
+	cp  ~/arch-setup/config/firefox/Profiles/Main/extensions.json  ~/.mozilla/firefox/Main/extensions.json
+	cp ~/arch-setup/config/firefox/Profiles/Hobby/extensions.json ~/.mozilla/firefox/Hobby/extensions.json
+	cp  ~/arch-setup/config/firefox/Profiles/UFSC/extensions.json  ~/.mozilla/firefox/UFSC/extensions.json
+	cp  ~/arch-setup/config/firefox/Profiles/Main/extension-settings.json ~/.mozilla/firefox/Main/extension-settings.json 
+	cp ~/arch-setup/config/firefox/Profiles/Hobby/extension-settings.json ~/.mozilla/firefox/Hobby/extension-settings.json
+	cp  ~/arch-setup/config/firefox/Profiles/UFSC/extension-settings.json ~/.mozilla/firefox/UFSC/extension-settings.json 
+	cp  ~/arch-setup/config/firefox/Profiles/Main/extension-preferences.json ~/.mozilla/firefox/Main/extension-preferences.json 
+	cp ~/arch-setup/config/firefox/Profiles/Hobby/extension-preferences.json ~/.mozilla/firefox/Hobby/extension-preferences.json
+	cp  ~/arch-setup/config/firefox/Profiles/UFSC/extension-preferences.json ~/.mozilla/firefox/UFSC/extension-preferences.json 
 
 
 backup:
-	cp  ~/.mozilla/firefox/Main/prefs.js  ~/MEGA/Main/prefs.js
-	cp ~/.mozilla/firefox/Hobby/prefs.js ~/MEGA/Hobby/prefs.js
-	cp  ~/.mozilla/firefox/UFSC/prefs.js  ~/MEGA/UFSC/prefs.js
-	cp  ~/.mozilla/firefox/Main/addons.json  ~/MEGA/Main/addons.json
-	cp ~/.mozilla/firefox/Hobby/addons.json ~/MEGA/Hobby/addons.json
-	cp  ~/.mozilla/firefox/UFSC/addons.json  ~/MEGA/UFSC/addons.json
-	cp  ~/.mozilla/firefox/Main/extensions.json  ~/MEGA/Main/extensions.json
-	cp ~/.mozilla/firefox/Hobby/extensions.json ~/MEGA/Hobby/extensions.json
-	cp  ~/.mozilla/firefox/UFSC/extensions.json  ~/MEGA/UFSC/extensions.json
-	cp  ~/.mozilla/firefox/Main/extension-settings.json  ~/MEGA/Main/extension-settings.json
-	cp ~/.mozilla/firefox/Hobby/extension-settings.json ~/MEGA/Hobby/extension-settings.json
-	cp  ~/.mozilla/firefox/UFSC/extension-settings.json  ~/MEGA/UFSC/extension-settings.json
-	cp  ~/.mozilla/firefox/Main/extension-preferences.json  ~/MEGA/Main/extension-preferences.json
-	cp ~/.mozilla/firefox/Hobby/extension-preferences.json ~/MEGA/Hobby/extension-preferences.json
-	cp  ~/.mozilla/firefox/UFSC/extension-preferences.json  ~/MEGA/UFSC/extension-preferences.json
+	cp  ~/.mozilla/firefox/Main/prefs.js  ~/arch-setup/config/firefox/Profiles/Main/prefs.js
+	cp ~/.mozilla/firefox/Hobby/prefs.js ~/arch-setup/config/firefox/Profiles/Hobby/prefs.js
+	cp  ~/.mozilla/firefox/UFSC/prefs.js  ~/arch-setup/config/firefox/Profiles/UFSC/prefs.js
+	cp  ~/.mozilla/firefox/Main/addons.json  ~/arch-setup/config/firefox/Profiles/Main/addons.json
+	cp ~/.mozilla/firefox/Hobby/addons.json ~/arch-setup/config/firefox/Profiles/Hobby/addons.json
+	cp  ~/.mozilla/firefox/UFSC/addons.json  ~/arch-setup/config/firefox/Profiles/UFSC/addons.json
+	cp  ~/.mozilla/firefox/Main/extensions.json  ~/arch-setup/config/firefox/Profiles/Main/extensions.json
+	cp ~/.mozilla/firefox/Hobby/extensions.json ~/arch-setup/config/firefox/Profiles/Hobby/extensions.json
+	cp  ~/.mozilla/firefox/UFSC/extensions.json  ~/arch-setup/config/firefox/Profiles/UFSC/extensions.json
+	cp  ~/.mozilla/firefox/Main/extension-settings.json  ~/arch-setup/config/firefox/Profiles/Main/extension-settings.json
+	cp ~/.mozilla/firefox/Hobby/extension-settings.json ~/arch-setup/config/firefox/Profiles/Hobby/extension-settings.json
+	cp  ~/.mozilla/firefox/UFSC/extension-settings.json  ~/arch-setup/config/firefox/Profiles/UFSC/extension-settings.json
+	cp  ~/.mozilla/firefox/Main/extension-preferences.json  ~/arch-setup/config/firefox/Profiles/Main/extension-preferences.json
+	cp ~/.mozilla/firefox/Hobby/extension-preferences.json ~/arch-setup/config/firefox/Profiles/Hobby/extension-preferences.json
+	cp  ~/.mozilla/firefox/UFSC/extension-preferences.json  ~/arch-setup/config/firefox/Profiles/UFSC/extension-preferences.json
 
